be3d30fdbb2d3928a992da1cb3c5fc048ba6bca3
akahige <akahigeg@gmail.com>
2013-10-22 18:46:29 +0900

    fix using old cookbook name for attirbutes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 
-if (node[:web_sync][:is_master] == true)
+if (node[:lsync][:is_master] == true)
   include_recipe "web-sync::master"
 else
   include_recipe "web-sync::slave"
